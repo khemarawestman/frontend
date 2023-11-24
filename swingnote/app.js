b4bb4fe06b941a319e485b084366c73b4fe368fa
khemarawestman <erikkhemarawestman@hotmail.se>
2023-11-24 12:24:13 +0100

    push

--- swingnote/app.js
+++ swingnote/app.js
@@ -1,60 +1,26 @@
-function fetchNotes() {
-  const username = document.getElementById("username").value;
-  if (!username) {
-    alert("Ange ett användarnamn.");
-    return;
-  }
+async function addNote() {
+  // Implement the code snippet for adding a note
+  // Get values from input fields, etc.
+}
 
-  fetch(
+async function fetchNotes() {
+  const username = document.getElementById("fetchUsername").value;
+  let response = await fetch(
     `https://o6wl0z7avc.execute-api.eu-north-1.amazonaws.com/api/notes/${username}`
-  )
-    .then((response) => response.json())
-    .then((notes) => displayNotes(notes))
-    .catch((error) => console.error("Error:", error));
+  );
+  let data = await response.json();
+
+  console.log(data);
 }
 
-function displayNotes(notes) {
-  const notesContainer = document.getElementById("notes");
-  notesContainer.innerHTML = "";
-  notes.forEach((note) => {
-    const noteElement = document.createElement("div");
-    noteElement.classList.add("note");
-    noteElement.innerHTML = `<h3>${note.title}</h3><p>${note.text}</p>`;
-    notesContainer.appendChild(noteElement);
-  });
+async function updateNote() {
+  // Implement the code snippet for updating a note
+  // You might need an input field to specify which note to update
 }
 
-function addNote() {
-  const title = document.getElementById("noteTitle").value;
-  const text = document.getElementById("noteText").value;
-  const username = document.getElementById("username").value;
-
-  if (!title || !text || !username) {
-    alert("Både titel, text och användarnamn måste anges.");
-    return;
-  }
-
-  const noteData = { username, title, text };
-
-  fetch("https://o6wl0z7avc.execute-api.eu-north-1.amazonaws.com/api/notes", {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json",
-    },
-    body: JSON.stringify(noteData),
-  })
-    .then((response) => response.json())
-    .then((data) => {
-      console.log("Success:", data);
-      fetchNotes(); // Uppdatera listan med anteckningar
-    })
-    .catch((error) => {
-      console.error("Error:", error);
-    });
-
-  document.getElementById("noteTitle").value = "";
-  document.getElementById("noteText").value = "";
+async function deleteNote() {
+  // Implement the code snippet for deleting a note
+  // You might need an input field to specify which note to delete
 }
 
-// Exempel på hur man kan uppdatera och radera anteckningar saknas i detta exempel.
-// Du behöver implementera dessa funktioner baserat på dina API-specifikationer.
+// Ensure these functions are properly linked to your HTML buttons
